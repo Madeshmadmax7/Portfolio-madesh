124d7e2675837d5a3b609a716c47f90a0cbe1640
MADMAX <amadesh702@gmail.com>
2025-12-03 12:38:01 +0530

    Refractor : Manifest json and service worker added

--- src/index.js
+++ src/index.js
@@ -12,3 +12,12 @@ root.render(
 );
 
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => console.log("Service worker registered:", reg))
+      .catch(err => console.log("Service worker registration failed:", err));
+  });
+}
